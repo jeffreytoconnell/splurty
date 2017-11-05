6ec7173965551e6787018381061cb6dc08488548
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-05 05:13:10 +0000

    Added form error

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,9 +8,12 @@ class QuotesController < ApplicationController
     end
 
     def create
-            Quote.create(quote_params)
-            redirect_to root_path
-    end
+        @quote = Quote.create(quote_params)
+        if @quote.invalid?
+          flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+        end
+        redirect_to root_path
+      end
 
     private
 
