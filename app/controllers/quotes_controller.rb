bda665a60603cef95b7013e2b279b0d36cdc68b2
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-06 01:50:33 +0000

    Finishing Touches

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,16 +3,15 @@ class QuotesController < ApplicationController
         @quote = Quote.order("RANDOM()").first
     end
 
-    def new
-        @quote = Quote.new
-    end
-
     def create
         @quote = Quote.create(quote_params)
         if @quote.invalid?
           flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
         end
         redirect_to root_path
+      end
+
+      def about
       end
 
     private
